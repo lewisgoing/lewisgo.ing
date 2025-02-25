455ca76af3d54acef363d9e6e03a6f0830e68bfa
lewisgoing <lgoing7@uw.edu>
2025-02-25 15:33:02 -0800

    Updated UX of dragging boxes

--- components/Bento.tsx
+++ components/Bento.tsx
@@ -181,10 +181,11 @@ export default function Bento() {
         cols={{ lg: 4, md: 4, sm: 2 }}
         rowHeight={rowHeight}
         isResizable={false}
+        // isDraggable={false} 
         onWidthChange={handleWidthChange}
         isBounded
         margin={[16, 16]}
-        // useCSSTransforms={false}
+        useCSSTransforms={false}
         onDragStart={(layout, oldItem, newItem, placeholder, e, element) =>
           handleDragStart(element)
         }
