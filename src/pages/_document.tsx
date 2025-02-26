4b8ef0047870877f536249c333c65a89b0e11f8e
lewisgoing <lgoing7@uw.edu>
2025-02-26 00:48:00 -0800

    added analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,16 @@
 // src/pages/_document.tsx
+'use cache'
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Document() {
   return (
     <Html lang="en">
+      
       <Head />
       <body>
         <Main />
+        <Analytics />
         <NextScript />
       </body>
     </Html>
