e03ff9c5795a7d386a4dce14f09b3ae1698b776b
lewisgoing <lgoing7@uw.edu>
2025-02-26 00:26:39 -0800

    setup for redis

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
     config.module.rules.push({
       test: /\.svg$/,
       use: [{ loader: "@svgr/webpack", options: { icon: true } }],
