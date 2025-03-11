539e156cdca5f842275295eefc9a290954f66392
lewisgoing <lgoing7@uw.edu>
2025-03-10 17:11:37 -0700

    Updated spotifybox tablet styles

--- components/Bento.tsx
+++ components/Bento.tsx
@@ -269,9 +269,7 @@ fill
   onMouseLeave={() => setIntroSilhouette(false)}
 >
   {/* Pass the full lanyard object to the SpotifyBox component */}
-    <SpotifyBox
-      onLoad={() => setIsSpotifyLoaded(true)}
-    />
+    <SpotifyBox    />
     {/* <Skeleton className="w-full h-full rounded-3xl z-[1]" /> */}
   
 </div>
